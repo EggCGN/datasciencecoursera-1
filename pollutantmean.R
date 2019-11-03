8ea79f8c3397274ff8745ffe0572bb8423dcdc21
Pierrorobis <pierre.robisson@gmail.com>
2019-11-03 23:27:28 +0100

    First function of week2 R programming lesson

---
+++ pollutantmean.R
@@ -0,0 +1,11 @@
+source("airpollutionutils.r")
+
+pollutantmean <- function(directory, pollutant, ids=1:332) {
+  values <- vector()
+  for (id in ids) {
+    df <- loadAirPollutCsv(directory, id)
+    pollutants <- df[[pollutant]][complete.cases(df[[pollutant]])]
+    values <- c(values, pollutants)
+  }
+  return(mean(values))
+}
